036c404d14400a2843ce1509c4ea3f12a2977b79
jonatasem <jonatas.em20@gmail.com>
2025-09-06 17:13:00 -0300

    feat: adicionando componente themesToggle, ligando contexto useTheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import ThemeToggle from "./components/Themes";
 import TodoList from "./components/TodoList";
+import { ThemeProvider } from "./context/ThemeContext";
 import { TodoProvider } from "./context/TodoContext";
 
 
 function App() {
   return (
     <>
-       <TodoProvider>
-          <div className="app-container">
-            <h1>Minha Lista de Tarefas</h1>
-            <TodoList />
-          </div>
-       </TodoProvider>
+       <ThemeProvider>
+          <TodoProvider>
+            <div className="app-container">
+              <ThemeToggle />
+              <h1>Minha Lista de Tarefas</h1>
+              <TodoList />
+            </div>
+        </TodoProvider>
+       </ThemeProvider>
     </>
   );
 }
